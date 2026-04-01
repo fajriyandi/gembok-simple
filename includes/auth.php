509e680f3bf03a575921cf846b2ea64cddd5c7cc
ALIJAYA JURAGAN PULSA <37974674+alijayanet@users.noreply.github.com>
2026-04-01 07:54:39 +0700

    Add files via upload

--- includes/auth.php
+++ includes/auth.php
@@ -20,6 +20,8 @@ require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/db.php';
 require_once __DIR__ . '/functions.php';
 
+date_default_timezone_set('Asia/Jakarta');
+
 // Start session if not started
 if (session_status() === PHP_SESSION_NONE) {
     ini_set('session.use_strict_mode', '1');
